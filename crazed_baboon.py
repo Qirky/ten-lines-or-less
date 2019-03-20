933645303635337a69f3feb1dd84c63aac57d0b6
bcgreen24 <bcgreen@gmail.com>
2019-03-19 23:44:18 -0500

    Update crazed_baboon.py

--- crazed_baboon.py
+++ crazed_baboon.py
@@ -1,4 +1,5 @@
 b1 >> fuzz([0, 2, 3, 5, 7, 9], dur=1/2)
 k1 >> play("x ").every(4, "amen")
 h1 >> play("- - -- -- [--]  ", amp=1.4)
+s1 >> play("     o     o oo     [oo]", amp=0.8)
 Clock.every(8, lambda: b1.shuffle())
